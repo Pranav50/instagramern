996b6a91ef9d31025b04f62980b3ae068642401c
Pranav50 <pranavpwr815@gmail.com>
2020-11-22 10:27:26 +0530

    All files added

--- config/keys.js
+++ config/keys.js
@@ -3,8 +3,3 @@ if(process.env.NODE_ENV==='production'){
 } else {
     module.exports = require('./dev')
 }
-
-module.exports = {
-    MONGO_URI: "mongodb+srv://pranavinsta:mJuLkvAOU5286bSO@cluster0.mhggr.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET: 'khsdfhjskdi12kj'
-}
